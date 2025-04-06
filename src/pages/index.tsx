bded8059df66b3fe5c139bef9c2a433d981e77b5
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-04-06 14:24:48 -0600

    Revert "frontend ready"
    This reverts commit ada22075834c792c48625d41be08715fc2262370.

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Bienvenida a Boxful Form</h1>
+      <p>Dirígete a <code>/form</code> para comenzar.</p>
+    </div>
+  );
+}
